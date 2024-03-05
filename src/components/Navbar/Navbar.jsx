df8675e4285ede894e5a1637aa72d3a446039b21
Parth Deore <parth.deore@icloud.com>
2024-03-05 22:31:07 +0530

    creating navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>Navbar</div>
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" width={24} height={24}/>
+        <Image src="/instagram.png" width={24} height={24}/>
+        <Image src="/tiktok.png" width={24} height={24}/>
+        <Image src="/youtube.png" width={24} height={24}/>
+      </div>
+      <div className={styles.logo}>parth.dev</div>
+      <div className={styles.links}>
+        <Link href="/">HomePage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+
+      </div>
+
+    </div>
   )
 }
 
