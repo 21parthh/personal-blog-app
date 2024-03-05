c7ce3b17b14eceb99f89414aa3c1fd66a8c7aeff
Parth Deore <parth.deore@icloud.com>
2024-03-05 09:51:14 +0530

    created responsive layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ import styles from "./homepage.module.css";
 
 export default function Home() {
   return <div>
-    <Link href="/">Hello</Link>
+    <Link href="/">
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi est ducimus blanditiis iusto impedit reprehenderit incidunt, distinctio sed dicta soluta ipsa nobis corrupti.</h1>
+       Maxime perspiciatis ducimus praesentium, iste provident velit?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam minima magnam fuga voluptatibus sunt officiis voluptatum, voluptates ipsum cum eius quis porro quibusdam dolores harum exercitationem, similique nam corrupti quod Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, ullam. Saepe, sint. Explicabo dicta optio id corporis ea neque, cupiditate nobis minus culpa architecto et ratione velit autem, dignissimos eius!Lorem</Link>
   </div>;
 }
