2ac49ee3b61f1096758759553127a720d31f479c
Parth Deore <parth.deore@icloud.com>
2024-03-06 22:31:25 +0530

    Context Api's in next JS for theme

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,3 +18,4 @@ export default function Home() {
     
   </div>);
 }
+//38.33
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-        <Image src="/facebook.png" width={24} height={24}/>
-        <Image src="/instagram.png" width={24} height={24}/>
-        <Image src="/tiktok.png" width={24} height={24}/>
-        <Image src="/youtube.png" width={24} height={24}/>
+        <Image src="/facebook.png" alt='facebook' width={24} height={24}/>
+        <Image src="/instagram.png" alt='instagram' width={24} height={24}/>
+        <Image src="/tiktok.png" alt='tiktok' width={24} height={24}/>
+        <Image src="/youtube.png" alt='youtube' width={24} height={24}/>
       </div>
       <div className={styles.logo}>parth.dev</div>
       <div className={styles.links}>
