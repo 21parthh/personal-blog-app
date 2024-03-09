cc3c2f86b078cf5e71185b93066b684185ad5875
Parth <parthdeore2829@gmail.com>
2024-03-09 12:15:42 +0530

    Featured Post UI Created

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import styles from './featured.module.css'
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div styles={''}>Featured</div>
+    <div styles={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Parth here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src={"/p1.jpeg"} alt='' fill className={styles.image}/>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>met neque inventore architecto. Mollitia, molestiae veritatis! Eligendi animi et sequi rem, est quod iure beatae quae eos, dolor repellendus mollitia alias?</p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
